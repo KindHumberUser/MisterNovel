f1395abb39006250216eebd615049b34dc0ae4ba
Sagar <rsagar@vt.edu>
2025-05-28 14:19:05 +0530

    Initial Commit: Spring Boot Novel App backend

---
+++ src/main/java/com/ranga/misternovel/MisterNovelApplication.java
@@ -0,0 +1,13 @@
+package com.ranga.misternovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisterNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisterNovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ranga/misternovel/MisterNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranga.misternovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisterNovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
