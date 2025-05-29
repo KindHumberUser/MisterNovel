d5ce23c8fac2139d4ddf87a57e35ea2c1fd8d12f
Sagar <rsagar@vt.edu>
2025-05-29 15:06:30 +0530

    Added Basic Test and testing plugins

--- src/test/java/com/ranga/misternovel/MisterNovelApplicationTests.java
+++ src/test/java/com/ranga/misternovel/MisterNovelApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ranga.misternovel;
 
+import com.ranga.misternovel.Controllers.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MisterNovelApplicationTests {
 
+    @Autowired
+    private HomeController homeController;
+
     @Test
     void contextLoads() {
+        assertThat(homeController).isNotNull();
     }
 
 }
